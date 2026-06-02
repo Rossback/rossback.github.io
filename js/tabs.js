b080b4c639dba0a3a45b3703f09618c0c4e63f11
Rossback <103798290+Rossback@users.noreply.github.com>
2026-06-02 14:14:00 -0700

    Still needs some touch-ups

---
+++ js/tabs.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded',
+    function () {
+
+        const tabsContainer = document.querySelector(".tabs-container");
+        const tabsList = tabsContainer.querySelector(".tabs-container-list");
+        const tabButtons = tabsList.querySelectorAll("a");
+        const tabPanels = tabsContainer.querySelectorAll(".tabs-panels > div");
+
+        tabButtons.forEach((tab, index) => {
+            if (index === 0) {
+
+            }
+            else {
+                tabPanels[index].setAttribute("hidden", "");
+            }
+        });
+        tabsContainer.addEventListener("click", (e) => {
+            const clickedTabButton = e.target.closest("a");
+            if (!clickedTabButton) {
+                return;
+            }
+            e.preventDefault();
+            const desiredElementId = clickedTabButton.getAttribute("href");
+            const currentClickedAnchorId = clickedTabButton.getAttribute("id");
+            const clickedButtonElement = tabsContainer.querySelector(desiredElementId);
+            const clickedAnchorElement = tabsList.querySelector("#" + currentClickedAnchorId);
+            tabPanels.forEach((panel) => {
+                panel.setAttribute("hidden", true);
+            });
+            tabButtons.forEach((button) => {
+                button.classList.remove("active");
+            })
+            clickedAnchorElement.classList.add("active");
+            clickedButtonElement.removeAttribute("hidden");
+        })
+    });
